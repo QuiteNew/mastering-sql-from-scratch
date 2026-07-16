594a855d13a92a5fa1449267957b449083f85c0f
QuiteNew <37884267+QuiteNew@users.noreply.github.com>
2026-07-16 10:22:46 +0200

    fixed commenting mistake
    Next time I will double check!

--- SQLBolt/02a_sql_lesson_2_queries_with_constraints.sql
+++ SQLBolt/02a_sql_lesson_2_queries_with_constraints.sql
@@ -2,7 +2,7 @@
 SQL Lesson 2: Queries with constraints (Pt. 1)
 
 Using the right constraints, find the information we need from the Movies table for each task below.
-/*
+*/
 
 
 -- Find the movie with a row id of 6 
